ef0ec79275baa6aa37bb96257746163076a6876a
ji55 <jsimbajs@gmail.com>
2021-10-26 12:47:33 +0900

    [FIX] merge frontend

---
+++ frontend/vue3/src/views/main/select-signup.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="signupBox">
+    <h1>내 손 안의 비서 DOBBY</h1>
+    <p>회원가입 하고 DOBBY를 만나보세요</p>
+
+    <router-link class="signupRouter" to="/signup">
+      <div>
+        <img src="@/assets/dobby.png" alt="" /> <br />
+        <p>Dobby 회원가입</p>
+      </div>
+    </router-link>
+    <router-link class="signupRouter" to="/sign">
+      <div>
+        <img src="@/assets/naver.png" alt="" />
+        <br />
+        <p>네이버 회원가입</p>
+      </div>
+    </router-link>
+    <router-link class="signupRouter" to="/sign">
+      <div>
+        <img src="@/assets/google.png" alt="" />
+        <br />
+        <p>구글 회원가입</p>
+      </div>
+    </router-link>
+    <br />
+    <router-link to="/main"><button>돌아가기</button></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectSignup',
+}
+</script>
+
+<style>
+.signupBox {
+  position: absolute;
+  width: 100%;
+  min-width: 200px;
+  max-width: 1500px;
+  left: 50%;
+  /* top: 50%; */
+  transform: translateX(-50%);
+}
+
+.signupBox > p {
+  margin-bottom: 50px;
+}
+
+.signupRouter {
+  text-decoration: none;
+}
+
+.signupRouter > div {
+  display: inline-block;
+  height: 330px;
+  width: 20%;
+  margin: 0 2%;
+  padding: 62px 20px 20px 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px #b3b3b3;
+  transition: all 0.1s linear;
+  cursor: pointer;
+}
+
+.signupRouter > div:hover,
+.signupBox > a > button:hover {
+  box-shadow: 0 0 10px #719ece;
+}
+
+.signupRouter > div > p {
+  margin-top: 70px;
+  font-size: 20px;
+  color: black;
+}
+
+.signupRouter > div > img {
+  width: 80%;
+  height: 200px;
+}
+
+.signupBox > a > button {
+  width: 300px;
+  height: 50px;
+  margin-top: 70px;
+  font-size: 20px;
+  font-family: 'Gowun Batang', serif !important;
+  color: white;
+  background-color: #a9c9de;
+  border: none;
+  border-radius: 4px;
+}
+
+@media screen and (max-width: 999px) {
+  .signupRouter > div {
+    display: block;
+    height: 40px;
+    width: 70%;
+    min-width: 200px;
+    max-width: 500px;
+    margin: 30px auto;
+    padding: 10px;
+  }
+
+  .signupRouter > div > img {
+    float: left;
+    width: 45px;
+    height: 40px;
+  }
+
+  .signupRouter > div > p {
+    margin-top: -20px;
+  }
+
+  .signupBox > a > button {
+    width: 200px;
+    margin-top: 10px;
+  }
+}
+
+@media screen and (max-width: 320px) {
+  .signupRouter > div > p {
+    font-size: 15px;
+    margin-top: -15px;
+  }
+}
+</style>
