d5216e5e14c0c6de4ebf96966ca05c1951f82152
root <root@ip-172-26-1-188.ap-northeast-2.compute.internal>
2021-10-30 14:49:07 +0000

    BE 로그인,BE 회원가입, BE 회원정보/ routes/ -> routes/controller 생성 / 로그인, 회원정보 찾기, 탈퇴 API 완료, router 와 Controller 분리, > 회원가입 시 데이터 유효성 검사 필요

--- backend/Dobby/routes/index.js
+++ backend/Dobby/routes/index.js
@@ -1,57 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var database = require("./../firebase/fbconfig.js");
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Dobby API Server" });
 });
-
-router.post('/signup', async  function(req,res,next){
-	const docRef = database.collection("users");
-
-	const data = {
-//		id : req.body["id"],
-		id : req.body["id"],
-		password : req.body["password"],
-		name : req.body["name"],
-		nickname : req.body["nickname"],
-		phone : req.body["phone"],
-		address : req.body["address"],
-		email : req.body["email"]
-	}
-
-	await docRef.add(data);
-	return res.json({
-		state : 201});
-});
-
-router.post('/gettest', async function(req,res,next){
-
-	console.log(req.body["id"]);
-	const docRef = database.collection("users");
-	const snapshot = await docRef.where("id","==",req.body["id"]).get();
-	
-	if(snapshot.empty){
-		return res.json({
-			msg : "no data"
-		});
-	}
-	else{
-		var redata = [];
-		snapshot.forEach((doc) => {
-			data = doc.data();
-			redata.push({
-				id : doc.id,
-				data : doc.data()
-			});
-		});
-		console.log(redata);
-		return res.json(redata);
-	}		
-});
-
 
 module.exports = router;
--- backend/Dobby/routes/users.js
+++ backend/Dobby/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const userControllder = require("./controller/userController");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
+router.post("/signup", userControllder.signUp);
+router.post("/login", userControllder.login);
+router.post("/findID", userControllder.findID);
+router.post("/findPW", userControllder.findPW);
+router.post("/changePW", userControllder.changePW);
+router.get("/checkDuplicateID", userControllder.checkDuplicateID);
+router.get("/checkDuplicateNick", userControllder.checkDuplicateNickname);
+router.get("/checkDuplicateEmail", userControllder.checkDuplicateEmail);
+router.delete("/withdraw", userControllder.withdrawUser);
 module.exports = router;
