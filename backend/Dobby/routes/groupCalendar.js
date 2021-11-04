3f73a3ee25b4433a94f7d27b202748012d600e51
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-11-04 15:47:03 +0900

    BE 그룹캘린더 / groupCalendar.js / 일단 서버가 안돌아가서 주석 처리

--- backend/Dobby/routes/groupCalendar.js
+++ backend/Dobby/routes/groupCalendar.js
@@ -3,16 +3,12 @@ var router = express.Router();
 
 const groupCalendarController = require("./controller/groupCalendarController");
 
-// router.get("/getAllgroups", groupController.getAllgroups);
-// router.get("/getGroup", groupController.getGroup);
-// router.post("/createGroup", groupController.createGroup);
-
-router.get("/getGroup", groupCalendarController.getGroup);
-router.get("/getCalendar", groupCalendarController.getCalendar);
-router.post("/createCalendar", groupCalendarController.createCalendar);
-router.put("/updateCalendar", groupCalendarController.updateCalendar);
-router.delete("/deleteCalendar", groupCalendarController.deleteCalendar);
-router.put("/checkCalendar", groupCalendarController.checkCalendar);
+// router.get("/getGroup", groupCalendarController.getGroup);
+// router.get("/getCalendar", groupCalendarController.getCalendar);
+// router.post("/createCalendar", groupCalendarController.createCalendar);
+// router.put("/updateCalendar", groupCalendarController.updateCalendar);
+// router.delete("/deleteCalendar", groupCalendarController.deleteCalendar);
+// router.put("/checkCalendar", groupCalendarController.checkCalendar);
 
 
 module.exports = router;
