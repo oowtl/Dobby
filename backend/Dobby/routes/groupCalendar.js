a60d615fd9498b5038b3abc6f5281a52b2ab4dab
DESKTOP-KVCQHCD\multicampus <shchae.dev@gmail.com>
2021-11-04 10:28:47 +0900

    BE 그룹캘린더 / groupCalendar.js, groupCalendarController.js / 파일 생성

---
+++ backend/Dobby/routes/groupCalendar.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+const groupController = require("./controller/groupCalendarController");
+
+// router.get("/getAllgroups", groupController.getAllgroups);
+// router.get("/getGroup", groupController.getGroup);
+// router.post("/createGroup", groupController.createGroup);
+
+module.exports = router;
