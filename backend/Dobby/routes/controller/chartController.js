480b4b585b556fe60f3e960f4fe725de132d8944
채승협 <tgs02159@icloud.com>
2021-11-09 16:36:11 +0900

    BE 차트 / chartController.js / 그룹 일정 차트 조회

--- backend/Dobby/routes/controller/chartController.js
+++ backend/Dobby/routes/controller/chartController.js
@@ -7,8 +7,8 @@ async function getPersonal(req, res, next) {
 
   if (valid) {
     const uid = req.body.uid;
-    const startDate = parseInt(req.body.startDate.replace(/\-/g,''));
-    const endDate = parseInt(req.body.endDate.replace(/\-/g,''));
+    const startDate = parseInt(req.body.startDate.replace(/\-/g, ""));
+    const endDate = parseInt(req.body.endDate.replace(/\-/g, ""));
     const calendarRef = admin
       .collection("users")
       .doc(uid)
@@ -23,8 +23,8 @@ async function getPersonal(req, res, next) {
       new Promise(async (resolve, reject) => {
         for (let doc of calendar.docs) {
           if (
-            parseInt(doc.data().startDate.replace(/\-/g,'')) >= startDate &&
-            parseInt(doc.data().endDate.replace(/\-/g,'')) <= endDate
+            parseInt(doc.data().startDate.replace(/\-/g, "")) >= startDate &&
+            parseInt(doc.data().endDate.replace(/\-/g, "")) <= endDate
           ) {
             totalNum += 1;
             if (totalCategory.empty) {
@@ -100,15 +100,100 @@ async function getGroup(req, res, next) {
   const valid = Auth.verifyToken(req.headers.authorization);
 
   if (valid) {
-      const uid = req.body.uid;
-      const gid = req.body.gid;
+    const uid = req.body.uid;
+    const gid = req.body.gid;
+    const startDate = parseInt(req.body.startDate.replace(/\-/g, ""));
+    const endDate = parseInt(req.body.endDate.replace(/\-/g, ""));
+    const calendarRef = admin
+      .collection("groups")
+      .doc(gid)
+      .collection("groupcalendar");
+    const calendar = await calendarRef.get();
 
-    } 
-    else {
-        res.status(403).json({
-            error: "Token is not vaild",
+    if (!calendar.empty) {
+      var totalCategory = [];
+      var checkCategory = [];
+      var totalNum = 0;
+      var checkNum = 0;
+
+      new Promise(async (resolve, reject) => {
+        for (let doc of calendar.docs) {
+          if (
+            parseInt(doc.data().startDate.replace(/\-/g, "")) >= startDate &&
+            parseInt(doc.data().endDate.replace(/\-/g, "")) <= endDate
+          ) {
+            totalNum += 1;
+            if (totalCategory.empty) {
+              totalCategory.push({
+                category: doc.category,
+                Num: 1,
+              });
+            } else {
+              let check = false;
+              for (let docu of totalCategory) {
+                if (docu.category == doc.category) {
+                  docu.Num += 1;
+                  check = true;
+                  break;
+                }
+              }
+              if (!check) {
+                totalCategory.push({
+                  category: doc.category,
+                  Num: 1,
+                });
+              }
+            }
+            for (let docum of doc.data().participant) {
+              if (docum.uid == uid) {
+                if (docum.completed == true) {
+                  checkNum += 1;
+                  if (checkCategory.empty) {
+                    checkCategory.push({
+                      category: doc.category,
+                      Num: 1,
+                    });
+                  } else {
+                    let check = false;
+                    for (let docu of checkCategory) {
+                      if (docu.category == doc.category) {
+                        docu.Num += 1;
+                        check = true;
+                        break;
+                      }
+                    }
+                    if (!check) {
+                      checkCategory.push({
+                        category: doc.category,
+                        Num: 1,
+                      });
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        resolve();
+      }).then(() => {
+        res.json({
+          totalNum: totalNum,
+          totalCategory: totalCategory,
+          checkNum: checkNum,
+          checkCategory: checkCategory,
+          msg: "그룹 일정 차트 조회 성공",
         });
+      });
+    } else {
+      res.status(401).json({
+        msg: "등록된 일정 정보가 없습니다.",
+      });
     }
+  } else {
+    res.status(403).json({
+      error: "Token is not vaild",
+    });
+  }
 }
 
 module.exports = {
