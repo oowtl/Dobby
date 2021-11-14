86994041d22b220d7311a91226bdea41874a19d0
Changhwan <ckdghks34@gmail.com>
2021-11-14 16:03:58 +0900

    Firebase Cloud Message / routes/controller/FCMController.js / 요청 들어온 fcm토큰 뿐 아니라 DB에 저장된 토큰 전체에 PUSH

--- backend/Dobby/routes/controller/FCMController.js
+++ backend/Dobby/routes/controller/FCMController.js
@@ -2,39 +2,45 @@ const { async } = require("@firebase/util");
 const { firebase_admin, admin } = require("./../../firebase/fbconfig");
 
 async function userPush(fcmtoken, uid, msg) {
-  const token = fcmtoken;
+  const userRef = admin.collection("users").doc(uid).collection("tokens");
+  const user = await userRef.get();
 
-  let message = {
-    notification: {
-      title: msg.title,
-      body: msg.body,
-    },
-    token: token,
-  };
+  if (user.empty) {
+    console.log("No token found for user : ", uid);
+    return false;
+  } else {
+    new Promise(async (resolve, reject) => {
+      for (let doc of user.docs) {
+        let message = {
+          notification: {
+            title: msg.title,
+            body: msg.body,
+          },
+          token: doc.data().token,
+        };
 
-  await firebase_admin
-    .messaging()
-    .send(message)
-    .then((res) => {
-      console.log("Successfully sent message : ", res);
-      return true;
-    })
-    .catch((err) => {
-      console.log("Error Sending message! : ", err);
-      return false;
+        await firebase_admin
+          .messaging()
+          .send(message)
+          .then((res) => {
+            console.log("Successfully sent message : ", res);
+            return true;
+          })
+          .catch((err) => {
+            console.log("Error Sending message! : ", err);
+            return false;
+          });
+      }
     });
+  }
 }
-
 async function groupPush(gid, msg) {
   const memberRef = admin.collection("groups").doc(gid).collection("members");
   const member = await memberRef.get();
 
   new Promise(async (resolve, reject) => {
     for (let doc of member.docs) {
-      var tokenRef = admin
-        .collection("users")
-        .doc(doc.data().uid)
-        .collection("tokens");
+      var tokenRef = admin.collection("users").doc(doc.data().uid).collection("tokens");
       var token = await tokenRef.get();
 
       for (let docu of token.docs) {
@@ -45,7 +51,7 @@ async function groupPush(gid, msg) {
           },
           token: docu.data().token,
         };
-      
+
         await firebase_admin
           .messaging()
           .send(message)
@@ -61,8 +67,6 @@ async function groupPush(gid, msg) {
     }
     resolve();
   });
-
-  
 }
 
 module.exports = {
