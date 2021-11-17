b3d1efa537e776b7c8ad13853a8bf7ef333667bd
DESKTOP-KVCQHCD\multicampus <hwangkj14@naver.com>
2021-11-17 09:47:19 +0900

    1117

--- frontend/vue3/src/App.vue
+++ frontend/vue3/src/App.vue
@@ -18,8 +18,8 @@
 
         <div>
           <ul>
-            <li v-for="groupList in info.groupLists" :key="groupList.gid" style="list-style: none;">
-              <div>
+            <li v-for="groupList in info.groupLists" :key="groupList.gid" style="list-style: none;" >
+              <div class="mulit-line-ellipsis">
                 <p @click="TogroupCallendar(groupList.gid)" style="display:inline; margin-right:7px; cursor:pointer;">{{ groupList.name }}</p>
                 <!-- <a @click="TogroupCallendar(groupList.gid)">{{ groupList.name }}</a> -->
                 <svg @click="ToGroup(groupList.gid)" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16" style="cursor:pointer;  margin-right:10px; float: right;">
@@ -48,11 +48,25 @@
         
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item><router-link to="/newgroup">New Group</router-link> </el-dropdown-item>
-            <!-- <el-dropdown-item><router-link to="/group">Group</router-link> </el-dropdown-item> -->
+            <!-- <el-dropdown-item><router-link to="/newgroup">New Group</router-link> </el-dropdown-item> -->
+            <el-dropdown-item><router-link to="/searchGroup">Group</router-link> </el-dropdown-item>
             <el-dropdown-item><router-link to="/calendar">Calendar</router-link> </el-dropdown-item>
             <!-- <el-dropdown-item><router-link to="/schedule">Schedule</router-link> </el-dropdown-item> -->
             <el-dropdown-item><router-link to="/chart">chart</router-link>  </el-dropdown-item>
+            <div>
+              <ul>
+                <li v-for="groupList in info.groupLists" :key="groupList.gid" style="list-style: none;">
+                  <div>
+                    <p @click="TogroupCallendar(groupList.gid)" style="display:inline; margin-right:7px; cursor:pointer;">{{ groupList.name }}</p>
+                    <!-- <a @click="TogroupCallendar(groupList.gid)">{{ groupList.name }}</a> -->
+                    <svg @click="ToGroup(groupList.gid)" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16" style="cursor:pointer; ">
+                      <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                    </svg>          
+                  </div>
+              
+                </li>
+              </ul>
+            </div>
             <el-dropdown-item><button @click="logout">logout</button>  </el-dropdown-item>
           </el-dropdown-menu>
         </template>
@@ -350,11 +364,18 @@ body,
   background-color: #DEB4B4;
 }
 
-@media (max-width: 1086px) {
+.mulit-line-ellipsis:before {
+  content: '...';
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+/* @media (max-width: 1086px) {
   svg {
     display: none;
   }
-}
+} */
 
 /* @media screen and (max-width: 730px) {
   .icon-bar {
