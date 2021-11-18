828318ecb1154c3980a42dd188084e83b2d30212
ji55 <jsimbajs@gmail.com>
2021-11-18 11:52:18 +0900

    소셜 로그인 header fcmtoken

--- frontend/vue3/src/views/main/select-signup.vue
+++ frontend/vue3/src/views/main/select-signup.vue
@@ -58,13 +58,21 @@ export default {
           localStorage.setItem('token', token)
           localStorage.setItem('uid', uid)
           axios
-            .post('https://k5d105.p.ssafy.io:3030/users/checkUserProvider', {
-              uid: uid,
-            })
+            .post(
+              'https://k5d105.p.ssafy.io:3030/users/checkUserProvider',
+              {
+                uid: uid,
+              },
+              {
+                headers: { FCMtoken: localStorage.getItem('FCMtoken') },
+              }
+            )
             .then((res) => {
               if (res.data.msg === '이미 등록된 회원입니다.') {
                 location.replace('/calendar')
               } else {
+                localStorage.removeItem('token', token)
+                localStorage.removeItem('uid', uid)
                 location.replace('/welcome')
               }
             })
@@ -88,13 +96,21 @@ export default {
           localStorage.setItem('token', token)
           localStorage.setItem('uid', uid)
           axios
-            .post('https://k5d105.p.ssafy.io:3030/users/checkUserProvider', {
-              uid: uid,
-            })
+            .post(
+              'https://k5d105.p.ssafy.io:3030/users/checkUserProvider',
+              {
+                uid: uid,
+              },
+              {
+                headers: { FCMtoken: localStorage.getItem('FCMtoken') },
+              }
+            )
             .then((res) => {
               if (res.data.msg === '이미 등록된 회원입니다.') {
                 location.replace('/calendar')
               } else {
+                localStorage.removeItem('token', token)
+                localStorage.removeItem('uid', uid)
                 location.replace('/welcome')
               }
             })
