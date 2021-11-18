7150d777bf7f3848d5a41831499401d23974a9b6
ji55 <jsimbajs@gmail.com>
2021-11-18 16:26:45 +0900

    일정 작성 권한 표시 아이콘 수정

--- frontend/vue3/src/views/group/group-info.vue
+++ frontend/vue3/src/views/group/group-info.vue
@@ -76,16 +76,38 @@
             >
             <span
               v-else-if="t.writer"
-              style="float: left; margin-left:2%; cursor: pointer;font-weight: 900; font-size: 20px;"
+              style="float: left; margin-left:2%; cursor: pointer;font-weight: 900; font-size: 20px; width:20px; margin-top:5px;"
               @click="handleWriter(t.nickname, true)"
-              >🖍</span
-            >
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1024 1024"
+                data-v-394d1fd8=""
+                @click="handleWriter(t.nickname, true)"
+              >
+                <path
+                  fill="currentColor"
+                  d="m174.72 855.68 135.296-45.12 23.68 11.84C388.096 849.536 448.576 864 512 864c211.84 0 384-166.784 384-352S723.84 160 512 160 128 326.784 128 512c0 69.12 24.96 139.264 70.848 199.232l22.08 28.8-46.272 115.584zm-45.248 82.56A32 32 0 0 1 89.6 896l58.368-145.92C94.72 680.32 64 596.864 64 512 64 299.904 256 96 512 96s448 203.904 448 416-192 416-448 416a461.056 461.056 0 0 1-206.912-48.384l-175.616 58.56z"
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="M512 563.2a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4zm192 0a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4zm-384 0a51.2 51.2 0 1 1 0-102.4 51.2 51.2 0 0 1 0 102.4z"
+                ></path></svg
+            ></span>
             <span
               v-else
-              style="color: lightgray; float: left; margin-left:2%; cursor: pointer; font-weight: 900; font-size: 20px;"
+              style="color: lightgray; float: left; margin-left:2%; cursor: pointer; font-weight: 900; font-size: 20px; width:20px; margin-top:5px;"
               @click="handleWriter(t.nickname, false)"
-              >🖍</span
-            >
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1024 1024"
+                data-v-394d1fd8=""
+                @click="handleWriter(t.nickname, false)"
+              >
+                <path
+                  fill="currentColor"
+                  d="m174.72 855.68 130.048-43.392 23.424 11.392C382.4 849.984 444.352 864 512 864c223.744 0 384-159.872 384-352 0-192.832-159.104-352-384-352S128 319.168 128 512a341.12 341.12 0 0 0 69.248 204.288l21.632 28.8-44.16 110.528zm-45.248 82.56A32 32 0 0 1 89.6 896l56.512-141.248A405.12 405.12 0 0 1 64 512C64 299.904 235.648 96 512 96s448 203.904 448 416-173.44 416-448 416c-79.68 0-150.848-17.152-211.712-46.72l-170.88 56.96z"
+                ></path></svg
+            ></span>
             <p
               style="display:inline-block; margin: 4px 0; cursor:pointer"
               @click="changeAdminBtn(t.nickname)"
